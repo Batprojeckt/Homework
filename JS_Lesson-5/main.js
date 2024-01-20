5468ecc8493fbff3d3fce6d3232235106fd5cd27
Batprojeckt <95305444+Batprojeckt@users.noreply.github.com>
2024-01-20 17:06:08 +0300

    add JS_Lesson-5

---
+++ JS_Lesson-5/main.js
@@ -0,0 +1,17 @@
+
+for (let i = 2; i <= 12; i = i + 2) {
+    console.log(i)
+}
+let i = 0
+while (i < 12) {
+    i = i + 2
+    console.log(i)
+}
+for (let i = 10; i >= 1; i--) {
+    console.log(i)
+}
+let cycle = 10
+while (cycle >= 1) {
+    cycle--
+    console.log(cycle)
+}
